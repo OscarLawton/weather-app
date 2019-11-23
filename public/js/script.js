4c9d20dd796c096169ed7870522ff54d17ad0683
Emmet <emmetlawton@gmail.com>
2019-11-23 13:22:04 +0000

    forecast on client side js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
 console.log("client side javascript file is loaded!!")
+
+/* fetch('https://puzzle.mead.io/puzzle').then((res) => {
+    res.json().then((data) => {
+        console.log(data);
+    })
+}) */
+
+fetch('http://localhost:3000/weather?address=London').then((res) => {
+    res.json().then((data) => {
+       if(data.err){
+           console.log(data.err);
+       } else {
+           console.log(data.address)
+           console.log(data.forecast);
+       }
+    })
+})
