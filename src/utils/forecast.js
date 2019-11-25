5ac410d80a2dfecc0102b19c177d5d100bff4633
Emmet <emmetlawton@gmail.com>
2019-11-25 23:08:20 +0000

    final touches

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,7 +17,7 @@ const forecast = (lat, long, callB) =>{
           console.log(res.body.longtitude);
           console.log(res.body.currently.summary); */
           c_temp = (temperature - 32 ) * 5/9;
-          callB(undefined, summary + ' It is currently ' + temperature + ' degress out. There is a ' + precipProbability + '% chance of rain.') 
+          callB(undefined, summary + ' It is currently ' + Math.floor(c_temp) + '° degress out. There is a ' + precipProbability + '% chance of rain.') 
           //callB(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
